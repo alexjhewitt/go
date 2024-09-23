68f323db622a795c20ffba86f60b0de7a6d4b38b
Alex <alexhewitt.j@gmail.com>
2024-09-23 10:13:19 -0500

    add in updated channels logic

--- stephen-grider-go/channels/main.go
+++ stephen-grider-go/channels/main.go
@@ -7,16 +7,23 @@ import (
 
 func main() {
 	links := []string{"http://google.com", "http://reddit.com", "http://golang.org", "http://amazon.com"}
+	ch := make(chan string)
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, ch)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-ch)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, ch chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		ch <- "Might be down I think"
 		return
 	}
 	fmt.Println(link, "is up!")
+	ch <- "Yup it is up"
 }
