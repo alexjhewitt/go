3b96e7e72d9ebec3d68ae6ecfbc4896eaa5e7b78
Alex <alexhewitt.j@gmail.com>
2024-09-23 08:28:26 -0500

    add code to read in text from file

--- stephen-grider-go/print-file/main.go
+++ stephen-grider-go/print-file/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	fileName := os.Args[1]
-	fmt.Println(fileName)
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	io.Copy(os.Stdout, file)
 }
