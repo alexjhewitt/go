91ba41ade8dfeb3abffe9180699e5c167f12e221
Alex <alexhewitt.j@gmail.com>
2024-09-24 08:40:12 -0500

    add in basic non-working code

---
+++ quiz-game/README.md
@@ -0,0 +1,12 @@
+# Quiz game
+A CLI quiz game that utilizes command-line flags, reading in a CSV, and timers.
+
+## Instructions
+- To run the game, be sure Go is installed on your system.
+- Then download the code from GitHub.
+- Navigate into the directory and run `go run main.go` to run the quiz game.
+
+## Flag options
+```
+-f = filename of your quiz data
+```
---
+++ quiz-game/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+	// Keep track of right/wrong answers
+	correctAnswers := 0
+	wrongAnswers := 0
+
+	// Flag creation
+	fileNamePtr := flag.String("f", "problems.csv", "Name of file with quiz questions")
+	flag.Parse()
+
+	// Read in CSV
+
+	// Ask next question
+
+	// Customize the csv file name with flag, but default to problems.csv
+
+	//
+}
