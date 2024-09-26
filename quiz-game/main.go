81b5a76ec579b2e975164940af4caa53986a083d
Alex <alexhewitt.j@gmail.com>
2024-09-26 07:53:07 -0500

    add starting game logic

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
 	// Keep track of right/wrong answers
-	correctAnswers := 0
-	wrongAnswers := 0
+	// correctAnswers := 0
+	// wrongAnswers := 0
 
 	// Flag creation
 	fileNamePtr := flag.String("f", "problems.csv", "Name of file with quiz questions")
 	flag.Parse()
 
-	// Read in CSV
+	// Read in CSV and work with data
+	file, err := os.Open(*fileNamePtr)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	r := csv.NewReader(file)
+	data, err := r.ReadAll()
+	for _, line := range data {
+		q := line[0]
+		// a := line[1]
+		fmt.Printf("What is %s?\t", q)
+	}
 
 	// Ask next question
 
